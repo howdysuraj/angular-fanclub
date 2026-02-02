fd23694bcee9a6181b82bb4e0543c77a4dab8d55
suraj4minoan <suraj.k@minoanexperience.com>
2026-02-02 22:13:44 +0530

    add footer component and update styles for light and dark themes

--- projects/fanclub/src/app/app.ts
+++ projects/fanclub/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from '../components/header/header';
 import {Hero} from '../components/hero/hero';
+import {Footer} from '../components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- projects/fanclub/src/pages/homepage/homepage.ts
+++ projects/fanclub/src/pages/homepage/homepage.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import {Hero} from "../../components/hero/hero";
-import {Vision} from '../../components/vision/vision';
-import {JoinCommunity} from '../../components/join-community/join-community';
+import { Hero } from '../../components/hero/hero';
+import { Vision } from '../../components/vision/vision';
+import { JoinCommunity } from '../../components/join-community/join-community';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'fanclub-homepage',
   imports: [
     Hero,
     Vision,
-    JoinCommunity
+    JoinCommunity,
   ],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss',
